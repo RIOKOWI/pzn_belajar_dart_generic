9c3df00669ade73a9846858aaef71d12ec9b974c
RIOKOWI <achyarrio2004@gmail.com>
2025-12-06 19:01:46 +0700

    first commit

---
+++ bin/pzn_belajar_dart_generic.dart
@@ -0,0 +1,5 @@
+import 'package:pzn_belajar_dart_generic/pzn_belajar_dart_generic.dart' as pzn_belajar_dart_generic;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pzn_belajar_dart_generic.calculate()}!');
+}
---
+++ test/pzn_belajar_dart_generic_test.dart
@@ -0,0 +1,8 @@
+import 'package:pzn_belajar_dart_generic/pzn_belajar_dart_generic.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
